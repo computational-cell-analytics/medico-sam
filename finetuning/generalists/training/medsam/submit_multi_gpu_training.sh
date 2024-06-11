776fdde102d9b05ca395d07e02c211ce4a693e83
anwai98 <anwai.archit@gmail.com>
2024-06-11 09:43:17 +0200

    Add slurm submission scripts

---
+++ finetuning/generalists/training/medsam/submit_multi_gpu_training.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=medico-sam
+#SBATCH -t 14-00:00:00
+#SBATCH --nodes=1
+#SBATCH -p grete:shared
+#SBATCH -G A100:8
+#SBATCH -A gzz0001
+#SBATCH --cpus-per-gpu 16
+#SBATCH --mem-per-gpu 96G
+#SBATCH --qos=14d
+#SBATCH --constraint=80gb
+
+source ~/.bashrc
+mamba activate sam
+python train_medical_multi_gpu_generalist.py -s /scratch/share/cidas/cca/models/medico-sam/multi_gpu
