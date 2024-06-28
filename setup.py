b307a9a7563ffb9896d80c212e038827db8458bc
Anwai Archit <52396323+anwai98@users.noreply.github.com>
2024-06-28 20:46:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='medico_sam',
     version="0.0.1",
-    description='micro-sam meets Medical Imaging',
+    description='MedicoSAM',
     author=['Anwai Archit', 'Constantin Pape'],
     url='https://user.informatik.uni-goettingen.de/~pape41/',
     packages=['medico_sam'],
