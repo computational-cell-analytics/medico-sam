8bd419f620d2457441ce585b8eb3990e79b24539
anwai98 <anwai.archit@gmail.com>
2024-11-12 00:55:52 +0100

    Update nnunet structure

--- semantic_segmentation/nnunetv2/common.py
+++ semantic_segmentation/nnunetv2/common.py
@@ -1,7 +1,3 @@
-import os
-
-import json
-
 from torch_em.data.datasets import medical
 
 
@@ -10,61 +6,45 @@ NNUNET_ROOT = "/scratch/share/cidas/cca/nnUNetv2"
 
 
 def _get_paths(path, dataset, split):
+    if split not in ['train', 'val', 'test']:
+        raise ValueError(f"'{split}' is not a valid split.")
+
+    # 2d datasets
     if dataset == "oimhs":
-        image_paths, gt_paths = medical.oimhs.get_oimhs_paths(path=path, split=split, download=False)
-
-    elif dataset == "cbis_ddsm":
-        if split == "val":
-            chosen_split = "Train"
-        else:
-            chosen_split = split.title()
-
-        image_paths, gt_paths = medical.cbis_ddsm.get_cbis_ddsm_paths(
-            path=path, split=chosen_split, task="Mass", tumour_type=None, download=True
-        )
-
-        if split == "train":
-            image_paths, gt_paths = image_paths[125:], gt_paths[125:]
-        elif split == "val":
-            image_paths, gt_paths = image_paths[:125], gt_paths[:125]
+        image_paths, gt_paths = medical.oimhs.get_oimhs_paths(path=path, split=split)
 
     elif dataset == "isic":
-        image_paths, gt_paths = medical.isic.get_isic_paths(path=path, split=split, download=False)
+        image_paths, gt_paths = medical.isic.get_isic_paths(path=path, split=split)
 
     elif dataset == "dca1":
-        image_paths, gt_paths = medical.dca1.get_dca1_paths(path=path, split=split, download=False)
+        image_paths, gt_paths = medical.dca1.get_dca1_paths(path=path, split=split)
+
+    elif dataset == "cbis_ddsm":
+        image_paths, gt_paths = medical.cbis_ddsm.get_cbis_ddsm_paths(path=path, split=split.title(), task="Mass")
 
     elif dataset == "drive":
-        image_paths, gt_paths = medical.drive.get_drive_paths(path=path, split=split, download=False)
+        image_paths, gt_paths = medical.drive.get_drive_paths(path=path, split=split)
 
     elif dataset == "piccolo":
-        if split == "val":
-            split = "validation"
-        image_paths, gt_paths = medical.piccolo.get_piccolo_paths(path=path, split=split, download=False)
+        image_paths, gt_paths = medical.piccolo.get_piccolo_paths(
+            path=path, split="validation" if split == "val" else split,
+        )
 
-    elif dataset == "btcv":
-        image_paths, gt_paths = medical.btcv._get_raw_and_label_paths(path=path, anatomy=["Abdomen"])
-        image_paths, gt_paths = image_paths["Abdomen"], gt_paths["Abdomen"]
+    elif dataset == "siim_acr":
+        image_paths, gt_paths = medical.siim_acr.get_siim_acr_paths(path=path, split=split)
 
-        if split == "train":
-            image_paths, gt_paths = image_paths[:18], gt_paths[:18]
-        elif split == "val":
-            image_paths, gt_paths = image_paths[18:22], gt_paths[18:22]
-        elif split == "test":
-            image_paths, gt_paths = image_paths[22:], gt_paths[22:]
+    elif dataset == "hil_toothseg":
+        image_paths, gt_paths = medical.hil_toothseg.get_hil_toothseg_paths(path=path, split=split)
 
-    elif dataset == "amos":
-        image_paths, gt_paths = medical.amos.get_amos_paths(path=path, split=split, modality="MRI", download=False)
+    elif dataset == "covid_qu_ex":
+        image_paths, gt_paths = medical.covid_qu_ex.get_covid_qu_ex_paths(path=path, split=split, task="lung")
+
+    # 3d datasets
+    elif dataset == "curvas":
+        image_paths, gt_paths = medical.curvas.get_curvas_paths(path=path, split=split)
 
     elif dataset == "osic_pulmofib":
-        image_paths, gt_paths = medical.osic_pulmofib.get_osic_pulmofib_paths(path=path, download=False)
-
-        if split == "train":
-            image_paths, gt_paths = image_paths[:75], gt_paths[:75]
-        elif split == "val":
-            image_paths, gt_paths = image_paths[75:85], gt_paths[75:85]
-        elif split == "test":
-            image_paths, gt_paths = image_paths[85:], gt_paths[85:]
+        image_paths, gt_paths = medical.osic_pulmofib.get_osic_pulmofib_paths(path=path, split=split)
 
     elif dataset == "sega":
         if split == "train":
@@ -73,13 +53,26 @@ def _get_paths(path, dataset, split):
             dchoice = "Dongyang"
         elif split == "test":
             dchoice = "KiTS"
-        else:
-            raise ValueError(split)
 
-        image_paths, gt_paths = medical.sega.get_sega_paths(path=path, data_choice=dchoice, download=False)
+        image_paths, gt_paths = medical.sega.get_sega_paths(path=path, data_choice=dchoice)
 
     elif dataset == "duke_liver":
-        image_paths, gt_paths = medical.duke_liver.get_duke_liver_paths(path=path, split=split, download=False)
+        image_paths, gt_paths = medical.duke_liver.get_duke_liver_paths(path=path, split=split)
+
+    elif dataset == "toothfairy":
+        image_paths, gt_paths = medical.toothfairy.get_toothfairy_paths(path=path, split=split, version="v1")
+
+    elif dataset == "oasis":
+        image_paths, gt_paths = medical.oasis.get_oasis_paths(path=path, split=split)
+
+    elif dataset == "lgg_mri":
+        image_paths, gt_paths = medical.lgg_mri.get_lgg_mri_paths(path=path, split=split)
+
+    elif dataset == "leg_3d_us":
+        image_paths, gt_paths = medical.leg_3d_us.get_leg_3d_us_paths(path=path, split=split)
+
+    elif dataset == "micro_usp":
+        image_paths, gt_paths = medical.micro_usp.get_micro_usp_paths(path=path, split=split)
 
     else:
         raise ValueError(dataset)
@@ -87,16 +80,125 @@ def _get_paths(path, dataset, split):
     return image_paths, gt_paths
 
 
-def create_json_files(trg_dir, dataset_name, write_json_function):
-    # now, let's create the 'dataset.json' file based on the available inputs
-    train_ids, val_ids = write_json_function(trg_dir=trg_dir, dataset_name=dataset_name)
+def _binarise_labels(labels):
+    labels = (labels > 1)
+    labels = labels.astype("uint8")
+    return labels
 
-    # let's try to store the splits file already
-    preprocessed_dir = os.path.join(trg_dir, "nnUNet_preprocessed", dataset_name)
-    os.makedirs(preprocessed_dir, exist_ok=True)
 
-    # we create custom splits for all folds, to fit with the expectation
-    all_split_inputs = [{'train': train_ids, 'val': val_ids} for _ in range(5)]
-    json_file = os.path.join(preprocessed_dir, "splits_final.json")
-    with open(json_file, "w") as f:
-        json.dump(all_split_inputs, f, indent=4)
+def _get_per_dataset_items(dataset):
+    preprocess_inputs, preprocess_labels = None, None
+    dataset_json_template = {
+        "name": nnunet_dataset_name,
+        "numTraining": len(val_ids) + len(train_ids),
+    }
+
+    # 2d dataset
+    if dataset == "oimhs":
+        file_suffix, transfer_mode = ".tif", "copy"
+
+        dataset_json_template["channel_names"] = {"0": "R", "1": "G", "2": "B"}
+        dataset_json_template["labels"] = {
+            "background": 0,
+            "choroid": 1,
+            "retina": 2,
+            "intraretinal_cysts": 3,
+            "macular_hole": 4,
+        }
+        dataset_json_template["description"] = "OIMHS: https://doi.org/10.1038/s41597-023-02675-1"
+
+    elif dataset == "isic":
+        file_suffix, transfer_mode = ".tif", "store"
+        preprocess_labels = _binarise_labels
+
+        dataset_json_template["channel_names"] = {"0": "R", "1": "G", "2": "B"}
+        dataset_json_template["labels"] = {"background": 0, "lesion": 1}
+        dataset_json_template["description"] = "ISIC: https://challenge.isic-archive.com/data/#2018"
+
+    elif dataset == "dca1":
+        file_suffix, transfer_mode = ".tif", "store"
+        preprocess_labels = _binarise_labels
+
+        dataset_json_template["channel_names"] = {"0": "X-Ray Angiography"}
+        dataset_json_template["labels"] = {"background": 0, "vessels": 1},
+        dataset_json_template["description"] = "DCA1: https://doi.org/10.1038/s41597-023-02675-1"
+
+    elif dataset == "cbis_ddsm":
+        file_suffix, transfer_mode = ".tif", "store"
+        preprocess_labels = _binarise_labels
+
+        dataset_json_template["channel_names"] = {"0": "Mammography"}
+        dataset_json_template["labels"] = {"background": 0, "mass": 1},
+        dataset_json_template["description"] = "CBIS-DDSM: https://doi.org/10.1038/sdata.2017.177"
+
+    elif dataset == "drive":
+        file_suffix, transfer_mode = ".tif", "store"
+        preprocess_labels = _binarise_labels
+
+        dataset_json_template["channel_names"] = {"0": "R", "1": "G", "2": "B"}
+        dataset_json_template["labels"] = {"background": 0, "vessels": 1},
+        dataset_json_template["description"] = "DRIVE: https://drive.grand-challenge.org/"
+
+    elif dataset == "piccolo":
+        file_suffix, transfer_mode = ".tif", "store"
+        preprocess_labels = _binarise_labels
+
+        dataset_json_template["channel_names"] = {"0": "R", "1": "G", "2": "B"}
+        dataset_json_template["labels"] = {"background": 0, "lesion": 1},
+        dataset_json_template["description"] = "PICCOLO: https://www.biobancovasco.bioef.eus/en/Sample-and-data-e-catalog/Databases/PD178-PICCOLO-EN1.html"  # noqa
+
+    elif dataset == "siim_acr":
+        ...
+
+    elif dataset == "hil_toothseg":
+        ...
+
+    elif dataset == "covid_qu_ex":
+        ...
+
+    # 3d dataset
+    elif dataset == "curvas":
+        ...
+
+    elif dataset == "osic_pulmofib":
+        file_suffix, transfer_mode = ".nii.gz", "copy"
+
+        dataset_json_template["channel_names"] = {"0": "CT"}
+        dataset_json_template["labels"] = {"background": 0, "heart": 1, "lung": 2, "trachea": 3}
+        dataset_json_template["description"] = "OSIC PulmoFib: https://www.kaggle.com/c/osic-pulmonary-fibrosis-progression/data"  # noqa
+
+    elif dataset == "sega":
+        file_suffix, transfer_mode = ".nii.gz", "copy"
+
+        dataset_json_template["channel_names"] = {"0": "CT"}
+        dataset_json_template["labels"] = {"background": 0, "aorta": 1}
+        dataset_json_template["description"] = "SegA: https://multicenteraorta.grand-challenge.org/"
+
+    elif dataset == "duke_liver":
+        file_suffix, transfer_mode = ".nii.gz", "copy"
+
+        dataset_json_template["channel_names"] = {"0": "MRI"}
+        dataset_json_template["labels"] = {"background": 0, "liver": 1}
+        dataset_json_template["description"] = "Duke Liver: https://zenodo.org/records/7774566"
+
+    elif dataset == "toothfairy":
+        ...
+
+    elif dataset == "oasis":
+        ...
+
+    elif dataset == "lgg_mri":
+        ...
+
+    elif dataset == "leg_3d_us":
+        ...
+
+    elif dataset == "micro_usp":
+        ...
+
+    else:
+        raise ValueError(dataset)
+
+    dataset_json_template["file_ending"] = file_suffix
+
+    return file_suffix, transfer_mode, dataset_json_template, preprocess_inputs, preprocess_labels
--- semantic_segmentation/nnunetv2/train_nnunetv2.py
+++ semantic_segmentation/nnunetv2/train_nnunetv2.py
@@ -1,73 +1,40 @@
-import os
-import warnings
 import argparse
 
-import torch
+from tukra.training import nnunet
+from tukra.utils import nnunet_utils
+
+from common import _get_paths, _get_per_dataset_items, DATA_ROOT
 
 
 NNUNET_ROOT = "/scratch/share/cidas/cca/nnUNetv2"
 
 DATASET_MAPPING_2D = {
     "oimhs": [201, "Dataset201_OIMHS"],
-    "isic": [203, "Dataset203_ISIC"],
-    "dca1": [204, "Dataset204_DCA1"],
-    "cbis_ddsm": [206, "Dataset206_CBISDDSM"],
-    "drive": [207, "Dataset207_DRIVE"],
-    "piccolo": [208, "Dataset208_PICCOLO"],
+    "isic": [202, "Dataset202_ISIC"],
+    "dca1": [203, "Dataset203_DCA1"],
+    "cbis_ddsm": [204, "Dataset204_CBISDDSM"],
+    "drive": [205, "Dataset205_DRIVE"],
+    "piccolo": [206, "Dataset206_PICCOLO"],
+    "siim_acr": [207, "Dataset207_SIIM_ACR"],
+    "hil_toothseg": [208, "Dataset208_HIL_ToothSeg"],
+    "covid_qu_ex": [209, "Dataset209_Covid_QU_EX"],
 }
 
 DATASET_MAPPING_3D = {
-    "btcv": [301, "Dataset301_BTCV"],
-    "osic_pulmofib": [303, "Dataset303_OSICPulmoFib"],
-    "sega": [304, "Dataset304_SegA"],
-    "duke_liver": [305, "Dataset305_DukeLiver"],
+    "curvas": [301, "Dataset301_Curvas"],
+    "osic_pulmofib": [302, "Dataset302_OSICPulmoFib"],
+    "sega": [303, "Dataset303_SegA"],
+    "duke_liver": [304, "Dataset304_DukeLiver"],
+    "toothfairy": [305, "Dataset305_ToothFairy"],
+    "oasis": [306, "Dataset306_OASIS"],
+    "lgg_mri": [307, "Dataset307_LGG_MRI"],
+    "leg_3d_us": [308, "Dataset308_Leg_3D_US"],
+    "micro_usp": [309, "Dataset309_MicroUSP"],
 }
 
 
-def declare_paths(nnunet_path: str):
-    """To let the system known of the path variables where the respective folders exist (important for all components)
-    """
-    warnings.warn(
-        "Make sure you have created the directories mentioned in this functions (relative to the root directory)"
-    )
-
-    os.environ["nnUNet_raw"] = os.path.join(nnunet_path, "nnUNet_raw")
-    os.environ["nnUNet_preprocessed"] = os.path.join(nnunet_path, "nnUNet_preprocessed")
-    os.environ["nnUNet_results"] = os.path.join(nnunet_path, "nnUNet_results")
-
-
-def preprocess_data(dataset_id):
-    # let's check the preprocessing first
-    cmd = f"nnUNetv2_plan_and_preprocess -d {dataset_id} -pl nnUNetPlannerResEncL --verify_dataset_integrity"
-    os.system(cmd)
-
-
-def train_nnunetv2(fold, dataset_name, dataset_id, dim):
-    _have_splits = os.path.exists(
-        os.path.join(NNUNET_ROOT, "nnUNet_preprocessed", dataset_name, "splits_final.json")
-    )
-    assert _have_splits, "The experiment expects you to create the splits yourself."
-
-    # train 2d / 3d_fullres nnUNet
-    gpus = torch.cuda.device_count()
-    cmd = f"nnUNet_compile=T nnUNet_n_proc_DA=8 nnUNetv2_train {dataset_id} {dim} {fold} -num_gpus {gpus} --c "
-    cmd += "-p nnUNetResEncUNetLPlans"
-    os.system(cmd)
-
-
-def predict_nnunetv2(fold, dataset_name, dataset_id, dim):
-    input_dir = os.path.join(NNUNET_ROOT, "test", dataset_name, "imagesTs")
-    assert os.path.exists(input_dir)
-
-    output_dir = os.path.join(NNUNET_ROOT, "test", dataset_name, "predictionTs")
-
-    cmd = f"nnUNetv2_predict -i {input_dir} -o {output_dir} -d {dataset_id} -c {dim} -f {fold} "
-    cmd += "-p nnUNetResEncUNetLPlans"
-    os.system(cmd)
-
-
 def main(args):
-    declare_paths(NNUNET_ROOT)
+    nnunet.declare_paths(NNUNET_ROOT)
 
     if args.dataset in DATASET_MAPPING_2D:
         dmap_base = DATASET_MAPPING_2D
@@ -81,13 +48,48 @@ def main(args):
     dataset_id, dataset_name = dmap_base[args.dataset]
 
     if args.preprocess:
-        preprocess_data(dataset_id=dataset_id)
+        (
+            file_suffix, transfer_mode, dataset_json_template, preprocess_inputs, preprocess_labels
+        ) = _get_per_dataset_items(dataset=args.dataset)
+
+        kwargs = {
+            "dataset_name": dataset_name,
+            "file_suffix": file_suffix,
+            "transfer_mode": transfer_mode,
+            "preprocess_inputs": preprocess_inputs,
+            "preprocess_labels": preprocess_labels,
+        }
+
+        train_image_paths, train_label_paths = _get_paths(path=DATA_ROOT, dataset=args.dataset, split="train")
+        train_ids = nnunet_utils.convert_dataset_for_nnunet_training(
+            image_paths=train_image_paths, gt_paths=train_label_paths, split="train", **kwargs
+        )
+
+        val_image_paths, val_label_paths = _get_paths(path=DATA_ROOT, split="train")
+        val_ids = nnunet_utils.convert_dataset_for_nnunet_training(
+            image_paths=val_image_paths, gt_paths=val_label_paths, split="val", **kwargs
+        )
+
+        nnunet_utils.create_json_files(
+            dataset_name=dataset_name,
+            file_suffix=file_suffix,
+            dataset_json_template=dataset_json_template,
+            train_ids=train_ids,
+            val_ids=val_ids,
+        )
+
+        nnunet.preprocess_data(dataset_id=dataset_id)
 
     if args.train:
-        train_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id, dim=dim)
+        nnunet.train_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id, dim=dim)
 
     if args.predict:
-        predict_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id, dim=dim)
+        test_image_paths, test_label_paths = _get_paths(path=DATA_ROOT, dataset=args.dataset, split="test")
+        nnunet_utils.convert_dataset_for_nnunet_training(
+            image_paths=test_image_paths, gt_paths=test_label_paths, split="test", **kwargs
+        )
+
+        nnunet.predict_nnunetv2(fold=args.fold, dataset_name=dataset_name, dataset_id=dataset_id, dim=dim)
 
 
 if __name__ == "__main__":
