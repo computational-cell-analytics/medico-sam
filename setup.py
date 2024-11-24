2c79f98131983ca7e2125691605f8bd313b938b9
anwai98 <anwai.archit@gmail.com>
2024-11-25 00:04:33 +0100

    Add version file to medico_sam module

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 
 from distutils.core import setup
 
+
+__version__ = runpy.run_path("medico_sam/__version__.py")["__version__"]
+
+
 setup(
     name='medico_sam',
     version="0.0.1",
-    description='MedicoSAM',
+    description='MedicoSAM: Segment Anything for Biomedical Images',
     author=['Anwai Archit', 'Constantin Pape'],
     url='https://user.informatik.uni-goettingen.de/~pape41/',
     packages=['medico_sam'],
 )
+
