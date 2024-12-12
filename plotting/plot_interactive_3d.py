141d9da1d5ccaf32405d1f9d590951c8aadacf9c
anwai98 <anwai.archit@gmail.com>
2024-12-12 14:32:10 +0100

    Update scripts to get results for sam2 interactive seg 3d

--- plotting/plot_interactive_3d.py
+++ plotting/plot_interactive_3d.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+from pathlib import Path
 
 import pandas as pd
 
@@ -7,21 +8,29 @@ import pandas as pd
 ROOT = "/mnt/vast-nhr/projects/cidas/cca/experiments/medico_sam/3d"
 
 
-def _get_results_per_dataset():
+def _get_sam2_results_per_dataset(dataset_name):
+    res_list = []
     for bpath in glob(os.path.join(ROOT, "*")):
         backbone = os.path.basename(bpath)
-        for mpath in glob(os.path.join(bpath, "*")):
-            model_name = os.path.basename(mpath)
-            for dpath in glob(os.path.join(mpath, "*")):
-                dataset = os.path.basename(dpath)
-                for res_path in glob(os.path.join(dpath, "results", "**", "*.csv")):
-                    res = pd.read_csv(res_path)
+        for res_path in glob(os.path.join(bpath, "hvit_b", dataset_name, "results", "**", "*.csv")):
+            res = pd.read_csv(res_path)
+            roi = res.columns[-1]
+            res_dict = {
+                "backbone": backbone,
+                "prompt_choice": Path(res_path).stem.split("_")[-1],
+                "type": roi,
+                "score": res.iloc[0][roi]
+            }
+            res_list.append(pd.DataFrame.from_dict([res_dict]))
 
-                    breakpoint()
+    res_df = pd.concat(res_list, ignore_index=True)
+    print(res_df)
+
+    breakpoint()
 
 
 def main():
-    _get_results_per_dataset()
+    _get_sam2_results_per_dataset("lgg_mri")
 
 
 if __name__ == "__main__":
