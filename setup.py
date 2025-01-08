dfe50e0453a26b1c5780031f42190b1bf62c1bca
Anwai Archit <anwai.archit@gmail.com>
2025-01-08 15:10:10 +0100

    Update version file in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ __version__ = runpy.run_path("medico_sam/__version__.py")["__version__"]
 
 setup(
     name='medico_sam',
-    version="0.0.1",
+    version=__version__,
     description='MedicoSAM: Segment Anything for Biomedical Images',
     author=['Anwai Archit', 'Constantin Pape'],
     url='https://user.informatik.uni-goettingen.de/~pape41/',
