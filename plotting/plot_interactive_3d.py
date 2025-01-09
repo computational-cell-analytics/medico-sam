2bd4f7ac0e9dd7a39db6d7a6e65cb483c5c7f52c
anwai98 <anwai.archit@gmail.com>
2025-01-09 15:40:28 +0100

    Update interactive 3d plots

--- plotting/plot_interactive_3d.py
+++ plotting/plot_interactive_3d.py
@@ -10,16 +10,18 @@ ROOT = "/mnt/vast-nhr/projects/cidas/cca/experiments/medico_sam/3d"
 
 DATASETS = {
     "lgg_mri": "LGG MRI (Low-Grade Glioma in MRI)",
-    # "leg_3d_us": "LEG 3D US",
     "microusp": "MicroUSP (Prostate in Micro-Ultrasound)",
-    "duke_liver": "Duke Liver (Liver in MRI)",
+    "duke_liver": "DLDS (Liver in MRI)",
+    # "kits": "KiTS (Kidney Tumor in CT)",
+    # "osic_pulmofib": "OSIC PulmoFib (Thoracic Organs in CT)",
+    # "segthy": "SegThy (Thyroid, Jugular Vein & Cartoid Artery and Veins in US)"
 }
 
 MODEL_MAPS = {
     "sam": "SAM",
-    "simplesam": "Simple FT",
+    "simplesam": "SimpleFT",
     "medsam": "MedSAM",
-    "sam2.0": "SAM2 (2.0)",
+    # "sam2.0": "SAM2 (2.0)",
     "sam2.1": "SAM2 (2.1)",
     "medico-sam-8g": r"$\bf{MedicoSAM}$",
 }
@@ -68,13 +70,19 @@ def _get_sam_results_per_dataset(dataset_name):
 
 
 def _get_plots():
-    fig, ax = plt.subplots(1, 3, figsize=(30, 15))
+    fig, ax = plt.subplots(3, 2, figsize=(30, 25))  # Create a 3x2 grid of subplots
+    ax = ax.flatten()  # Flatten to make it 1D for easier indexing
+
+    bar_width = 0.4
+    scale = 1.2  # Scale factor for font sizes
+    fontsize_base = int(16 * scale)
+    fontsize_legend = int(16 * scale)
+    fontsize_axis_label = int(20 * scale)
 
     for i, (dname, dmap) in enumerate(DATASETS.items()):
         # Extract results per dataset
         df1 = _get_sam_results_per_dataset(dname)
         df2 = _get_sam2_results_per_dataset(dname)
-
         df = pd.concat([df1, df2], ignore_index=True)
 
         methods = MODEL_MAPS.keys()
@@ -86,20 +94,26 @@ def _get_plots():
             res = {"method": _method}
 
             if "box" in base_res['prompt_choice'].tolist():
-                res["box"] = df[(df['backbone'] == _method) & (df['prompt_choice'] == 'box')]['score'].iloc[0]
+                try:
+                    res["box"] = df[(df['backbone'] == _method) & (df['prompt_choice'] == 'box')]['score'].iloc[0]
+                except IndexError:
+                    res["box"] = 0
             else:
                 res["box"] = 0
 
             if "point" in base_res['prompt_choice'].tolist():
-                res["point"] = df[(df['backbone'] == _method) & (df['prompt_choice'] == 'point')]['score'].iloc[0]
+                try:
+                    res["point"] = df[(df['backbone'] == _method) & (df['prompt_choice'] == 'point')]['score'].iloc[0]
+                except IndexError:
+                    res["point"] = 0
             else:
                 res["point"] = 0
 
             scores.append(res)
 
-        bar_width = 0.4
         x = range(len(methods))
 
+        # Plot bars for "Box" and "Point"
         ax[i].bar(
             [pos - bar_width / 2 for pos in x], [s.get("box") for s in scores],
             width=bar_width, label='Box', color="#FCDE9C", edgecolor="grey",
@@ -110,18 +124,22 @@ def _get_plots():
         )
 
         ax[i].set_xticks(x)
-        ax[i].set_xticklabels(labels, rotation=45, ha='right', fontsize=16)
-        ax[i].tick_params(axis='y', labelsize=16)  # Set y-tick label size
-        ax[i].set_title(dmap, fontweight="bold", fontsize=16)
+        ax[i].set_xticklabels(labels, fontsize=fontsize_base)
+        ax[i].tick_params(axis='y', labelsize=fontsize_base)
+        ax[i].set_title(dmap, fontweight="bold", fontsize=fontsize_base)
+
+    # Turn off unused subplots
+    for idx in range(len(DATASETS), len(ax)):
+        ax[idx].axis("off")
 
     handles, labels = ax[0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='lower center', fontsize=16, ncol=2)
+    fig.legend(handles, labels, loc='lower center', fontsize=fontsize_legend, ncol=2)
 
     plt.text(
-        x=-15.4, y=0.24, s="Dice Similarity Coefficient", rotation=90, fontweight="bold", fontsize=20
+        x=-1.2, y=1.275, s="Dice Similarity Coefficient", rotation=90, fontweight="bold", fontsize=fontsize_axis_label
     )
 
-    plt.subplots_adjust(top=0.9, bottom=0.125, right=0.95, left=0.05, wspace=0.1)
+    plt.subplots_adjust(top=0.9, bottom=0.125, right=0.95, left=0.05, hspace=0.25, wspace=0.1)
     plt.savefig("./fig_5_interactive_segmentation_3d_per_dataset.png", bbox_inches="tight")
     plt.savefig("./fig_5_interactive_segmentation_3d_per_dataset.svg", bbox_inches="tight")
 
