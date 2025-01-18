1a9fdb172d1a943bbe614c012015e13b6a284d74
anwai98 <anwai.archit@gmail.com>
2025-01-18 17:09:33 +0100

    Add other datasets

--- plotting/qualitative_comparison/compare_models.py
+++ plotting/qualitative_comparison/compare_models.py
@@ -60,35 +60,67 @@ def fetch_data_loaders(dataset_name):
     def _trafo(x, y):
         return x, y
 
-    if dataset_name == "camus":
+    def _raw_trafo(raw):
+        raw = raw.transpose(2, 0, 1)
+        return raw
+
+    def _label_trafo(labels):
+        labels = labels.transpose(2, 0, 1)
+        return labels
+
+    if dataset_name == "camus":  # echocardiography
         loader = datasets.get_camus_loader(
             path=os.path.join(ROOT, "camus"), batch_size=1, patch_shape=(1, 512, 512), ndim=2, resize_inputs=True,
             sampler=sampler, shuffle=True, label_transform=connected_components, transform=_trafo,
         )
 
-    elif dataset_name == "uwaterloo_skin":
+    elif dataset_name == "uwaterloo_skin":  # dermoscopy
         loader = datasets.get_uwaterloo_skin_loader(
             path=os.path.join(ROOT, "uwaterloo_skin"), batch_size=1, patch_shape=(512, 512), resize_inputs=True,
             sampler=sampler, shuffle=True, label_transform=connected_components, transform=_trafo,
         )
 
-    elif dataset_name == "montgomery":
+    elif dataset_name == "montgomery":  # x-ray
         loader = datasets.get_montgomery_loader(
             path=os.path.join(ROOT, "montgomery"), batch_size=1, patch_shape=(512, 512), resize_inputs=True,
             sampler=sampler, shuffle=True, label_transform=connected_components, transform=_trafo,
         )
 
-    elif dataset_name == "sega":
-        loader = datasets.get_sega_loader(
-            path=os.path.join(ROOT, "sega"), batch_size=1, patch_shape=(1, 512, 512), ndim=2, resize_inputs=True,
-            data_choice="Dongyang", sampler=sampler, label_transform=connected_components, transform=_trafo,
-            shuffle=True,
-        )
-
-    elif dataset_name == "piccolo":
+    elif dataset_name == "piccolo":  # nbi
         loader = datasets.get_piccolo_loader(
             path=os.path.join(ROOT, "piccolo"), batch_size=1, patch_shape=(512, 512), split="test", resize_inputs=True,
             sampler=sampler, shuffle=True, label_transform=connected_components, transform=_trafo,
+        )
+
+    elif dataset_name == "cbis_ddsm":  # mammography
+        loader = datasets.get_cbis_ddsm_loader(
+            path=os.path.join(ROOT, "cbis_ddsm"), batch_size=1, patch_shape=(512, 512), resize_inputs=True, task="Mass",
+            split="Test", tumour_type="MALIGNANT", shuffle=True, label_transform=connected_components, transform=_trafo,
+        )
+
+    elif dataset_name == "duke_liver":  # mri
+        loader = datasets.get_duke_liver_loader(
+            path=os.path.join(ROOT, "duke_liver"), batch_size=1, patch_shape=(1, 512, 512), ndim=2, resize_inputs=True,
+            split="test", transform=_trafo, shuffle=True, sampler=sampler,
+            raw_transform=_raw_trafo, label_transform=_label_trafo,
+        )
+
+    elif dataset_name == "papila":  # fundus
+        loader = datasets.get_papila_loader(
+            path=os.path.join(ROOT, "papila"), batch_size=1, patch_shape=(512, 512), resize_inputs=True, split="test",
+            task="disc", sampler=sampler, shuffle=True,
+        )
+
+    elif dataset_name == "han-seg":  # ct
+        loader = datasets.get_han_seg_loader(
+            path=os.path.join(ROOT, "han-seg"), batch_size=1, patch_shape=(1, 512, 512), ndim=2, resize_inputs=True,
+            sampler=sampler, label_transform=connected_components,
+        )
+
+    elif dataset_name == "microusp":  # micro-us
+        loader = datasets.get_micro_usp_loader(
+            path=os.path.join(ROOT, "microusp"), batch_size=1, patch_shape=(1, 512, 512), ndim=2,
+            split="test", resize_inputs=True, sampler=sampler, shuffle=True,
         )
 
     else:
@@ -97,21 +129,22 @@ def fetch_data_loaders(dataset_name):
     return loader
 
 
-def main():
-    datasets = ["sega"]
-
-    for dname in datasets:
-        compare_experiments_for_dataset(
-            dataset_name=dname,
-            experiment_folder="./figures",
-            standard_model="vit_b",
-            finetuned_model="vit_b_medicosam",
-            intermediate_model="vit_b_medsam",
-            checkpoint1=None,
-            checkpoint2="/mnt/vast-nhr/projects/cidas/cca/models/medico-sam/multi_gpu/checkpoints/vit_b/medical_generalist_sam_multi_gpu/best_exported.pt",  # noqa
-            checkpoint3="/mnt/vast-nhr/projects/cidas/cca/models/medsam/original/medsam_vit_b.pth",
-        )
+def main(args):
+    compare_experiments_for_dataset(
+        dataset_name=args.dataset,
+        experiment_folder="./figures",
+        standard_model="vit_b",
+        finetuned_model="vit_b_medicosam",
+        intermediate_model="vit_b_medsam",
+        checkpoint1=None,
+        checkpoint2="/mnt/vast-nhr/projects/cidas/cca/models/medico-sam/multi_gpu/checkpoints/vit_b/medical_generalist_sam_multi_gpu/best_exported.pt",  # noqa
+        checkpoint3="/mnt/vast-nhr/projects/cidas/cca/models/medsam/original/medsam_vit_b.pth",
+    )
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", type=str, required=True)
+    args = parser.parse_args()
+    main(args)
