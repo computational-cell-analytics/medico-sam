5d2975dbbc002d6ece403bd5d33e112f447c9e05
Anwai Archit <anwai.archit@gmail.com>
2025-01-22 22:49:06 +0100

    Add example scripts for all relevant tasks

---
+++ medico_sam/sample_data.py
@@ -0,0 +1,11 @@
+"""Sample medical imaging data.
+
+You can change the download location for the sample data and model weights
+by setting the environment variable: MEDICOSAM_CACHEDIR
+
+By default sample data is downloaded to a folder named 'medico_sam/sample_data'.
+inside your default cache directory, eg:
+    * Mac: ~/Library/Caches/<AppName>
+    * Unix: ~/.cache/<AppName> or the value of the XDG_CACHE_HOME environment variable, if defined.
+    * Windows: C:\Users\<user>\AppData\Local\<AppAuthor>\<AppName>\Cache
+"""
