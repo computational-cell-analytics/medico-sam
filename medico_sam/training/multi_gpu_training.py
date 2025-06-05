e4f44366d6a9cee0735a0fe681932ffda9c5b164
anwai98 <anwai.archit@gmail.com>
2025-06-05 23:35:55 +0200

    Finalize multi-gpu joint training

--- medico_sam/training/multi_gpu_training.py
+++ medico_sam/training/multi_gpu_training.py
@@ -1,14 +1,14 @@
 from functools import partial
-from typing import Dict, Any, Optional, Callable
+from typing import Dict, Any, Callable
 
 import torch
 import torch.utils.data
 
-import torch_em
 from torch_em.multi_gpu_training import setup, cleanup, _create_data_loader, DDP
 
+from micro_sam.instance_segmentation import get_unetr
 
-# Write your own version, move it to medico sam, so we have custom multi-gpu joint training
+
 def _train_impl(
     rank: int,
     world_size: int,
@@ -20,37 +20,42 @@ def _train_impl(
     val_dataset_kwargs: Dict[str, Any],
     loader_kwargs: Dict[str, Any],
     iterations: int,
+    optimizer_callable: Callable[[Any], torch.optim.Optimizer],
+    optimizer_kwargs: Dict[str, Any],
+    lr_scheduler_callable: Callable[[Any], torch.optim.lr_scheduler._LRScheduler],
+    lr_scheduler_kwargs: Dict[str, Any],
+    trainer_callable: Callable,
     find_unused_parameters: bool = True,
-    optimizer_callable: Optional[Callable[[Any], torch.optim.Optimizer]] = None,
-    optimizer_kwargs: Optional[Dict[str, Any]] = None,
-    lr_scheduler_callable: Optional[Callable[[Any], torch.optim.lr_scheduler._LRScheduler]] = None,
-    lr_scheduler_kwargs: Optional[Dict[str, Any]] = None,
-    trainer_callable: Optional[Callable] = None,
     **kwargs
 ):
     assert "device" not in kwargs
     print(f"Running DDP on rank {rank}.")
     setup(rank, world_size)
 
-    model = model_callable(**model_kwargs).to(rank)
-    ddp_encoder = DDP(model, device_ids=[rank], find_unused_parameters=find_unused_parameters)
-    ddp_decoder = DDP(decoder, ...)
+    model, state = model_callable(return_state=True, **model_kwargs)
+    unetr = get_unetr(image_encoder=model.sam.image_encoder, decoder_state=state.get("decoder_state", None))
 
-    if optimizer_callable is not None:
-        optimizer = optimizer_callable(model.parameters() + decoder.parameters(), **optimizer_kwargs)
-        kwargs["optimizer"] = optimizer
-        if lr_scheduler_callable is not None:
-            lr_scheduler = lr_scheduler_callable(optimizer, **lr_scheduler_kwargs)
-            kwargs["lr_scheduler"] = lr_scheduler
+    model.to(rank)
+    unetr.to(rank)
+
+    ddp_model = DDP(model, device_ids=[rank], find_unused_parameters=find_unused_parameters)
+    ddp_unetr = DDP(unetr, device_ids=[rank], find_unused_parameters=find_unused_parameters)
+
+    model_params = [params for params in model.parameters()]
+    decoder_params = [params for param_name, params in unetr.named_parameters() if not param_name.startswith("encoder")]
+
+    optimizer = optimizer_callable(model_params + decoder_params, **optimizer_kwargs)
+    kwargs["optimizer"] = optimizer
+
+    lr_scheduler = lr_scheduler_callable(optimizer, **lr_scheduler_kwargs)
+    kwargs["lr_scheduler"] = lr_scheduler
 
     train_loader = _create_data_loader(train_dataset_callable, train_dataset_kwargs, loader_kwargs, world_size, rank)
     val_loader = _create_data_loader(val_dataset_callable, val_dataset_kwargs, loader_kwargs, world_size, rank)
 
-    if trainer_callable is None:
-        trainer_callable = torch_em.default_segmentation_trainer
-
     trainer = trainer_callable(
         model=ddp_model,
+        unetr=ddp_unetr,
         train_loader=train_loader,
         val_loader=val_loader,
         device=rank,
@@ -71,12 +76,12 @@ def train_multi_gpu(
     val_dataset_kwargs: Dict[str, Any],
     loader_kwargs: Dict[str, Any],
     iterations: int,
+    optimizer_callable: Callable[[Any], torch.optim.Optimizer],
+    optimizer_kwargs: Dict[str, Any],
+    lr_scheduler_callable: Callable[[Any], torch.optim.lr_scheduler._LRScheduler],
+    lr_scheduler_kwargs: Dict[str, Any],
+    trainer_callable: Callable,
     find_unused_parameters: bool = True,
-    optimizer_callable: Optional[Callable[[Any], torch.optim.Optimizer]] = None,
-    optimizer_kwargs: Optional[Dict[str, Any]] = None,
-    lr_scheduler_callable: Optional[Callable[[Any], torch.optim.lr_scheduler._LRScheduler]] = None,
-    lr_scheduler_kwargs: Optional[Dict[str, Any]] = None,
-    trainer_callable: Optional[Callable] = None,
     **kwargs
 ) -> None:
     """Run data parallel training on multiple local GPUs via torch.distributed.
@@ -95,12 +100,12 @@ def train_multi_gpu(
         val_dataset_kwargs: Keyword arguments for `val_dataset_callable`.
         loader_kwargs: Keyword arguments for the torch data loader.
         iterations: Number of iterations to train for.
-        find_unused_parameters: Whether to find unused parameters of the model to exclude from the optimization.
         optimizer_callable: Function or class to create the optimizer.
         optimizer_kwargs: Keyword arguments for `optimizer_callable`.
         lr_scheduler_callable: Function or class to create the learning rate scheduler.
         lr_scheduler_kwargs: Keyword arguments for `lr_scheduler_callable`.
         trainer_callable: Function or class to create the trainer.
+        find_unused_parameters: Whether to find unused parameters of the model to exclude from the optimization.
         kwargs: Keyword arguments for `trainer_callable`.
     """
     world_size = torch.cuda.device_count()
@@ -114,12 +119,12 @@ def train_multi_gpu(
         val_dataset_kwargs=val_dataset_kwargs,
         loader_kwargs=loader_kwargs,
         iterations=iterations,
-        find_unused_parameters=find_unused_parameters,
         optimizer_callable=optimizer_callable,
         optimizer_kwargs=optimizer_kwargs,
         lr_scheduler_callable=lr_scheduler_callable,
         lr_scheduler_kwargs=lr_scheduler_kwargs,
         trainer_callable=trainer_callable,
+        find_unused_parameters=find_unused_parameters,
         **kwargs
     )
     torch.multiprocessing.spawn(train, args=(world_size,), nprocs=world_size, join=True)
